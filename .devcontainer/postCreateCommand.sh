43da8681a601db4e80b6387c345a5b883f2cad19
Sam Erde <samuel.erde@gmail.com>
2025-10-30 13:43:59 -0500

    Remove postCreateCommand and delete associated script

--- .devcontainer/postCreateCommand.sh
+++
@@ -1,6 +0,0 @@
-# Install xdg-utils to open authentication in device browser
-sudo apt-get update
-sudo apt-get install -y xdg-utils
-
-# Build website and report
-cd ./website && npm i && cd ../report && npm i
